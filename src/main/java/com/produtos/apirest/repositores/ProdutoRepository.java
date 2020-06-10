0a23fcb34eaab685072cc4deafb5772fe75e5644
Hipertrofia <jansen@dcc.ufrj.br>
2020-06-10 18:00:16 -0300

    adicionando os métodos  de listagem de produtos e busca  de um produto especifico

--- src/main/java/com/produtos/apirest/repositores/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repositores/ProdutoRepository.java
@@ -1,5 +1,10 @@
 package com.produtos.apirest.repositores;
 
-public interface ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.produtos.apirest.models.Produto;
+
+public interface ProdutoRepository extends JpaRepository <Produto, Long>{
+
+	Produto findById(long id);
 }
